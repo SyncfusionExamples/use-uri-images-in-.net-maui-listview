e26e3968fffaa9c24edc8d8a2656489de53ae5cc
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2022-04-13 11:15:06 +0530

    MAUI-1681 Sample added

---
+++ ListViewMaui/ListViewMaui/Model/Contact.cs
@@ -0,0 +1,70 @@
+using Microsoft.Maui.Controls;
+using System.ComponentModel;
+
+
+namespace ListViewMaui
+{
+    public class Contacts : INotifyPropertyChanged
+    {
+        private string contactName;
+        private string contactNumber;
+        private string contactImage;
+
+        public Contacts(string name, string number)
+        {
+            contactName = name;
+            contactNumber = number;
+        }
+
+        public Contacts()
+        {
+
+        }
+
+        public string ContactName
+        {
+            get { return contactName; }
+            set
+            {
+                if (contactName != value)
+                {
+                    contactName = value;
+                    this.RaisedOnPropertyChanged("ContactName");
+                }
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set
+            {
+                if (contactNumber != value)
+                {
+                    contactNumber = value;
+                    this.RaisedOnPropertyChanged("ContactNumber");
+                }
+            }
+        }
+
+        public string ContactImage
+        {
+            get { return this.contactImage; }
+            set
+            {
+                this.contactImage = value;
+                this.RaisedOnPropertyChanged("ContactImage");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+    }
+}
---
+++ ListViewMaui/ListViewMaui/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Maui.Controls;
+using System.Text;
+
+
+namespace ListViewMaui
+{
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        public ObservableCollection<Contacts> contactsinfo { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ContactsViewModel()
+        {
+            contactsinfo = new ObservableCollection<Contacts>();
+            GenerateInfo();
+        }
+
+        public void GenerateInfo()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 40; i++)
+            {
+                var contact = new Contacts(CustomerNames[i], r.Next(720, 799).ToString() + " - " + r.Next(3010, 3999).ToString());
+                contact.ContactImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/220px-User_icon_2.svg.png"; //Your Url goes here
+                contactsinfo.Add(contact);
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Jack",
+            "Liz",
+            "Zeke",
+            "Noah",
+            "Pete",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson",
+            "Mason",
+            "Liam",
+            "Jacob",
+            "Jayden",
+            "Ethan",
+            "Noah",
+            "Lucas",
+            "Logan",
+            "Caleb",
+            "Caden",
+            "Jack",
+            "Ryan",
+            "Connor",
+            "Michael",
+            "Elijah",
+            "Brayden",
+            "Benjamin",
+            "Nicholas",
+            "Alexander",
+            "William",
+            "Matthew",
+            "James",
+            "Landon",
+            "Nathan",
+            "Dylan",
+            "Evan",
+            "Luke",
+            "Andrew",
+            "Gabriel",
+            "Gavin",
+            "Joshua",
+            "Owen",
+            "Daniel",
+            "Carter",
+            "Tyler",
+            "Cameron",
+            "Christian",
+            "Wyatt",
+            "Henry",
+            "Eli",
+            "Joseph",
+            "Max",
+            "Isaac",
+            "Samuel",
+            "Anthony",
+            "Grayson",
+            "Zachary",
+            "David",
+            "Christopher",
+            "John",
+            "Isaiah",
+            "Levi",
+            "Jonathan",
+            "Oliver",
+            "Chase",
+            "Cooper" ,
+            "Tristan",
+            "Colton",
+            "Austin",
+            "Colin",
+            "Charlie",
+            "Dominic",
+            "Parker",
+            "Hunter",
+            "Thomas",
+            "Alex",
+            "Ian",
+            "Jordan",
+            "Cole",
+            "Julian",
+            "Aaron",
+            "Carson",
+            "Miles",
+            "Blake",
+            "Brody",
+            "Adam",
+            "Sebastian",
+            "Adrian",
+            "Nolan",
+            "Sean",
+            "Riley",
+            "Bentley",
+            "Xavier",
+            "Hayden",
+            "Jeremiah",
+            "Jason",
+            "Jake",
+            "Asher",
+            "Micah",
+            "Jace",
+            "Brandon",
+            "Josiah",
+            "Hudson",
+            "Nathaniel",
+            "Bryson",
+            "Ryder",
+            "Justin",
+            "Bryce",
+        };
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
